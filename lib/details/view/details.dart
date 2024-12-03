249a4c9a8f63fcda6d862769d04bcf036cded3ea
Salohiddin Sayfiddinov <salokhiddin.sayfiddinov@mail.com>
2024-12-03 18:10:17 +0500

    minor changes

--- lib/details/view/details.dart
+++ lib/details/view/details.dart
@@ -12,150 +12,153 @@ class DetailsScreen extends StatefulWidget {
 class _DetailsScreenState extends State<DetailsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFF212121),
-      appBar: AppBar(
-        backgroundColor: Colors.grey,
-        title: const Text(
-          "Details",
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+    return BlocProvider(
+      create: (context) => PlusCubit(),
+      child: Scaffold(
+        backgroundColor: const Color(0xFF212121),
+        appBar: AppBar(
+          backgroundColor: Colors.grey,
+          title: const Text(
+            "Details",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+          ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 50.0),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.black45,
-                        blurRadius: 15,
-                        offset: Offset(5, 5))
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 50.0),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black45,
+                          blurRadius: 15,
+                          offset: Offset(5, 5))
+                    ],
+                  ),
+                  width: double.infinity,
+                  height: 250,
+                  clipBehavior: Clip.hardEdge,
+                  child: Image.network(
+                    "https://static.insales-cdn.com/images/products/1/8159/524238815/LIS_4318.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Gourmet Burger",
+                      style: TextStyle(
+                          fontSize: 32,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "\$10.5",
+                      style: TextStyle(
+                          fontSize: 32,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
                   ],
                 ),
-                width: double.infinity,
-                height: 250,
-                clipBehavior: Clip.hardEdge,
-                child: Image.network(
-                  "https://static.insales-cdn.com/images/products/1/8159/524238815/LIS_4318.jpg",
-                  fit: BoxFit.cover,
+                const SizedBox(height: 10),
+                Text(
+                  "This juicy, gourmet burger is crafted from premium beef, topped with fresh veggies, melted cheese, and our special sauce, all sandwiched in a freshly baked brioche bun. It's the ultimate burger experience for anyone craving something rich and flavorful.",
+                  style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 16,
+                      fontStyle: FontStyle.italic,
+                      height: 1.6),
+                  textAlign: TextAlign.center,
                 ),
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Gourmet Burger",
-                    style: TextStyle(
-                        fontSize: 32,
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 200,
+                      height: 70,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
                         color: Colors.white,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    "\$10.5",
-                    style: TextStyle(
-                        fontSize: 32,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 10),
-              Text(
-                "This juicy, gourmet burger is crafted from premium beef, topped with fresh veggies, melted cheese, and our special sauce, all sandwiched in a freshly baked brioche bun. It's the ultimate burger experience for anyone craving something rich and flavorful.",
-                style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 16,
-                    fontStyle: FontStyle.italic,
-                    height: 1.6),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 200,
-                    height: 70,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.black12,
-                            blurRadius: 10,
-                            offset: Offset(2, 2))
-                      ],
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          IconButton(
-                            color: const Color(0xFFB71C1C),
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.exposure_minus_1,
-                              color: Colors.black,
-                              size: 30,
-                            ),
-                          ),
-                          BlocBuilder<PlusCubit, int>(
-                              builder: (BuildContext context, int state) {
-                            return Text(
-                              state.toString(),
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 28,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            );
-                          }),
-                          IconButton(
-                            color: const Color(0xFF00C853),
-                            onPressed: () {
-                              context.read<PlusCubit>().plusOne();
-                            },
-                            icon: const Icon(
-                              Icons.plus_one,
-                              color: Colors.black,
-                              size: 30,
-                            ),
-                          ),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 10,
+                              offset: Offset(2, 2))
                         ],
                       ),
-                    ),
-                  ),
-                  const SizedBox(width: 20),
-                  SizedBox(
-                    width: 200,
-                    height: 80,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.grey,
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 30, vertical: 12),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30)),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            IconButton(
+                              color: const Color(0xFFB71C1C),
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.exposure_minus_1,
+                                color: Colors.black,
+                                size: 30,
+                              ),
+                            ),
+                            BlocBuilder<PlusCubit, int>(
+                                builder: (BuildContext context, int state) {
+                              return Text(
+                                state.toString(),
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              );
+                            }),
+                            IconButton(
+                              color: const Color(0xFF00C853),
+                              onPressed: () {
+                                context.read<PlusCubit>().plusOne();
+                              },
+                              icon: const Icon(
+                                Icons.plus_one,
+                                color: Colors.black,
+                                size: 30,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                      child: const Text(
-                        "Add to Cost",
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(width: 20),
+                    SizedBox(
+                      width: 200,
+                      height: 80,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.grey,
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 30, vertical: 12),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30)),
+                        ),
+                        child: const Text(
+                          "Add to Cost",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
