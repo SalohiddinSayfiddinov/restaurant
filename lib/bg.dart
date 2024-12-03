c2f77f3a039b4ab640ff6b5fbfb62ec18bea1843
muhammadayub122 <mirbositovmuhamadaub496@gmail.com>
2024-12-03 17:33:34 +0500

    added home page

---
+++ lib/bg.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color background = Color(0xFF201520);
+  static const Color waite = Color(0xFFFFFFFF);
+  static const Color iconcolor = Color(0xFFEFE3C8);
+  static const Color tabcolor = Color.fromRGBO(112, 67, 65, 0.3);
+  static const Color iconcolor05 = Color.fromRGBO(239, 227, 200, 0.5);
+}
